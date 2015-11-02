41990ce74fc922837b0a06b14d759f40e5d286a0
Jacob Young <jacob@jacobyoung.me>
2015-11-01 23:46:09 -0800

    Removed semicolons from site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,28 +1,28 @@
-(function(){
-  var themeToggle = document.getElementById('theme-toggle');
-  var darkThemeCookie = Cookies.get('dark');
-  var isDarkTheme = false;
-  if(darkThemeCookie) {
-    isDarkTheme = true;
-    enableDarkTheme();
+(function (){
+  var themeToggle = document.getElementById('theme-toggle')
+  var darkThemeCookie = Cookies.get('dark')
+  var isDarkTheme = false
+  if (darkThemeCookie) {
+    isDarkTheme = true
+    enableDarkTheme()
   }
 
-  if (themeToggle) themeToggle.addEventListener('click', switchTheme);
+  if (themeToggle) themeToggle.addEventListener('click', switchTheme)
 
   function switchTheme(e) {
-    e.preventDefault();
-    if(!isDarkTheme) enableDarkTheme();
-    else disableDarkTheme();
+    e.preventDefault()
+    if (!isDarkTheme) enableDarkTheme()
+    else disableDarkTheme()
   }
-  function enableDarkTheme() {
-    isDarkTheme = true;
-    Cookies.set('dark', 'true', {expiration: 90, path: '/'});
-    document.body.classList.add('dark-background');
-    document.body.classList.add('dark-text-color');
+  function enableDarkTheme () {
+    isDarkTheme = true
+    Cookies.set('dark', 'true', {expiration: 90, path: '/'})
+    document.body.classList.add('dark-background')
+    document.body.classList.add('dark-text-color')
   }
-  function disableDarkTheme() {
-    isDarkTheme = false;
-    Cookies.remove('dark');
-    document.body.className = '';
+  function disableDarkTheme () {
+    isDarkTheme = false
+    Cookies.remove('dark')
+    document.body.className = ''
   }
-})();
+})()
