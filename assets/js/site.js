3cf851f56ce00c69fbaf72c140fc8ce154bfc226
Jacob Young <jacob@jacobyoung.me>
2015-12-01 01:12:09 -0600

    Change theme color from orange. Typography changes.
    The main theme color was changed from orange to a deep black/grey color.
    Dark theme switching is still present and has changed some values.
    
    The primary text-font is Muli from brick.im. The headers are all Lato
    font.
    
    Additionally, the site.js code cleared some excess codeblock logic which
    was replaced by pure CSS.
    
    Also, the external links function was modified to use .hostname
    instead of regular expressiosn

--- assets/js/site.js
+++ assets/js/site.js
@@ -6,8 +6,6 @@
 
 function handleTheme () {
   var themeToggle = document.getElementById('theme-toggle')
-  var codeFences = document.getElementsByClassName('highlight')
-  var codeBlocks = document.getElementsByTagName('code')
   // Grabs document.cookie with name "dark" using the Cookie.js library
   // https://github.com/js-cookie/js-cookie
   var darkThemeCookie = Cookies.get('dark')
@@ -28,45 +26,20 @@ function handleTheme () {
   function enableDarkTheme () {
     isDarkTheme = true
     Cookies.set('dark', 'true', {expiration: 90, path: '/'})
-    document.body.classList.add('dark-background')
-    document.body.classList.add('dark-text-color')
+    document.body.classList.add('dark')
     themeToggle.innerHTML = 'Light Theme'
-    if (codeFences) {
-      forEach(codeFences, function (elem, index) {
-        elem.classList.toggle('dark-code-background')
-      })
-    }
-    if (codeBlocks) {
-      forEach(codeBlocks, function (elem, index) {
-        if (!elem.classList.contains('language')) {
-          elem.classList.toggle('dark-code-background')
-        }
-      })
-    }
   }
   function disableDarkTheme () {
     isDarkTheme = false
     Cookies.remove('dark')
-    document.body.className = ''
+    document.body.classList.remove('dark')
     themeToggle.innerHTML = 'Dark Theme'
-    if (codeFences) {
-      forEach(codeFences, function (elem, index) {
-        elem.classList.toggle('dark-code-background')
-      })
-    }
-    if (codeBlocks) {
-      forEach(codeBlocks, function (elem, index) {
-        if (!elem.classList.contains('language')) {
-          elem.classList.toggle('dark-code-background')
-        }
-      })
-    }
   }
 }
 
 // Adds attribute "target=_blank" to links to all external sites
 function handleExternalLinks () {
-  var host = location.host
+  var host = location.hostname
   var allLinks = document.querySelectorAll('a')
   forEach(allLinks, function (elem, index) {
     checkExternalLink(elem, host)
